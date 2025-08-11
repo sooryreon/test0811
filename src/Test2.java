5f35bf9ce7ca72d15a30da66ce1474e5719f08f3
chol4723 <chg4723@naver.com>
2025-08-11 12:14:11 +0900

    가나다라마바사, 10까지 반복문

--- src/Test2.java
+++ src/Test2.java
@@ -2,5 +2,10 @@
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("AB");
+		System.out.println("가나다라마바사");
+		for(int i=0; i<=10; i++) {
+			System.out.println(1);
+		}
+		
 	}
 }
